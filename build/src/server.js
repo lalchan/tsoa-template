ad52863fbc52951a4adbd9f01c19a8b0a1cdb8db
lalchan <scorpiclalchan@gmail.com>
2024-04-25 12:35:38 +0545

    removed build directory

--- build/src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const initialize_1 = require("./initialize");
-const routes_1 = require("../build/routes");
-const app = (0, initialize_1.initializeApp)();
-app.use((0, routes_1.RegisterRoutes)(app));
-app.listen(3000, () => {
-    "Application is running on port 3000";
-});
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import { initializeApp } from "./initialize";
 import { RegisterRoutes } from "../build/routes";
 import { Config } from "./config/env";
+import {
+	errorHandler,
+	notFoundHandler,
+} from "./common/requestHandlers/expressError";
 
 const app = initializeApp();
 RegisterRoutes(app);
 
 const port = Config.port;
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 app.listen(port, () => {
 	`Application is running on port ${port}`;
 });
